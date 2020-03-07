26b641455affff408c383660c0d1c38c5b3cbd43
MirZadeh.A <m_amirarsalan@outlook.com>
2020-03-07 13:34:31 +0330

    Fixed Operation Buttons (Add, Delete, ...) Ckeditor is now updated to latest version

--- src/Hatra/wwwroot/lib/ckeditor/config.js
+++ src/Hatra/wwwroot/lib/ckeditor/config.js
@@ -1,13 +1,10 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	 config.language = 'fa';
-    // config.uiColor = '#AADC6E';
-    config.skin = "bootstrapck";
-    config.resize_dir = 'both';
-    config.extraPlugins= 'divarea';
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
